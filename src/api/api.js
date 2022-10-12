7ee9e591bd4c4ee78597504d0024911173c41ba1
Zevi <abc627097@gmail.com>
2022-10-12 18:16:34 -0400

    issue fix

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+const travel_advisor_api_key=''; //your access key to the travel advisor api
+
 const getPlaceData = async (type, sw, ne) => {
     try {
         const {data:{data}} = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
@@ -10,7 +12,7 @@ const getPlaceData = async (type, sw, ne) => {
                 tr_longitude: ne.lng,
             },
             headers: {
-                'X-RapidAPI-Key': 'b710f4a96dmsh148ce80f0b7c43ep1c38e5jsna85ea015f097',
+                'X-RapidAPI-Key': travel_advisor_api_key,
                 'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
             }
         });
